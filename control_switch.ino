65a4257a8fa23a4158dff68132803f7aa287b29e
wen lin li <willie300300@yahoo.com.tw>
2019-10-30 22:30:33 +0800

    加入開關控制

--- control_switch.ino
+++ control_switch.ino
@@ -11,6 +11,15 @@ int relay4 = 10;
 int relay5 = 9;
 int relay6 = 8;
 int relay7 = 7;
+int button1 = 6;
+int button2 = 5;
+int lastButtonState1 = LOW;
+int lastButtonState2 = LOW;
+long lastDebounceTime1 = 0;
+long lastDebounceTime2 = 0;                    
+long debounceDelay = 50;
+int bs1;
+int bs2;
 
 
 void setup() {
@@ -21,17 +30,59 @@ void setup() {
     pinMode(relay5, OUTPUT);
     pinMode(relay6, OUTPUT);
     pinMode(relay7, OUTPUT);
+    pinMode(button1, INPUT);
+    pinMode(button2, INPUT);
+    
 }
 
 
 void loop() {
-    control_relay1();
-    control_relay2();
-    control_relay3();
-    control_relay4();
-    control_relay5();
-    control_relay6();
-    control_relay7();         
+    int button1_State = digitalRead(button1);
+    int button2_State = digitalRead(button2);
+    
+    // ======按鈕一開始======
+    
+    if (button1_State != lastButtonState1){
+        lastDebounceTime1 = millis();
+    }
+    if ((millis() - lastDebounceTime1) > debounceDelay) {
+        if (button1_State != bs1) {
+            bs1 = button1_State;
+            if (bs1 == HIGH) {
+                control_relay1();  
+                control_relay2();
+                control_relay3();
+                control_relay4();
+                control_relay5();
+                control_relay6();
+                control_relay7();
+            }
+        }
+    }
+    lastButtonState1 = button1_State;
+
+    // ======按鈕二開始======
+
+    if (button2_State != lastButtonState2){
+        lastDebounceTime2 = millis();
+    }
+    if ((millis() - lastDebounceTime2) > debounceDelay) {
+        if (button2_State != bs2) {
+            bs2 = button1_State;
+            if (bs2 == HIGH) {
+                control_relay1();  
+                control_relay2();
+                control_relay3();
+                control_relay4();
+                control_relay5();
+                control_relay6();
+                control_relay7();
+            }
+        }
+    }
+    lastButtonState2 = button2_State;
+    
+        
 }
 
 
