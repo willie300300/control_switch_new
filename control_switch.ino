155eccddc5923ee0f7e89e5b07ca90ea68094203
wen lin li <willie300300@yahoo.com.tw>
2019-10-30 20:57:27 +0800

    增加function

--- control_switch.ino
+++ control_switch.ino
@@ -25,73 +25,67 @@ void setup() {
 
 
 void loop() {
-    digitalWrite(relay1,HIGH);
-    delay(1000);
-    digitalWrite(relay1,LOW);
-    delay(1000);
-    
-    digitalWrite(relay2,HIGH);
-    delay(1000);
-    digitalWrite(relay2,LOW);
-    delay(1000);
-            
-    digitalWrite(relay3,HIGH);
-    delay(1000);
-    digitalWrite(relay3,LOW);
-    delay(1000);
-
-    digitalWrite(relay4,HIGH);
-    delay(1000);
-    digitalWrite(relay4,LOW);
-    delay(1000);
-
-    digitalWrite(relay5,HIGH);
-    delay(1000);
-    digitalWrite(relay5,LOW);
-    delay(1000);
-
-    digitalWrite(relay6,HIGH);
-    delay(1000);
-    digitalWrite(relay6,LOW);
-    delay(1000);   
-
-    digitalWrite(relay7,HIGH);
-    delay(1000);
-    digitalWrite(relay7,LOW);
-    delay(1000);                             
+    control_relay1();
+    control_relay2();
+    control_relay3();
+    control_relay4();
+    control_relay5();
+    control_relay6();
+    control_relay7();         
 }
 
 
 void control_relay1(){
-
+    digitalWrite(relay1,HIGH);
+    delay(1000);
+    digitalWrite(relay1,LOW);
+    delay(1000);
 }
 
 
 void control_relay2(){
-
+    digitalWrite(relay2,HIGH);
+    delay(1000);
+    digitalWrite(relay2,LOW);
+    delay(1000);
 }
 
 
 void control_relay3(){
-
+    digitalWrite(relay3,HIGH);
+    delay(1000);
+    digitalWrite(relay3,LOW);
+    delay(1000);
 }
 
 
 void control_relay4(){
-
+    digitalWrite(relay4,HIGH);
+    delay(1000);
+    digitalWrite(relay4,LOW);
+    delay(1000);
 }
 
 
 void control_relay5(){
-
+    digitalWrite(relay5,HIGH);
+    delay(1000);
+    digitalWrite(relay5,LOW);
+    delay(1000);
 }
 
 
 void control_relay6(){
-
+    digitalWrite(relay6,HIGH);
+    delay(1000);
+    digitalWrite(relay6,LOW);
+    delay(1000); 
 }
 
 
 void control_relay7(){
-
+    digitalWrite(relay7,HIGH);
+    delay(1000);
+    digitalWrite(relay7,LOW);
+    delay(1000); 
 }
